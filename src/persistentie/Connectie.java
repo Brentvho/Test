46427ece51919f78c3b6a2195916342019fc0c6d
Brent Van Hoecke <brent.vanhoecke.t9195@student.hogent.be>
2015-02-10 11:47:51 +0100

    Starten van project

---
+++ src/persistentie/Connectie.java
@@ -0,0 +1,9 @@
+package persistentie;
+
+public class Connectie {
+
+    /*
+     * INFO: Hieronder dien je uiteraard het schema, de username en het paswoord nog aan te passen.
+     */
+    public static final String JDBC_URL = "jdbc:mysql://localhost:3306/dobbelsteen?user=root&password=root";
+}
